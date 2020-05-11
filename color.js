9f55fcc74c80a92f8703be39a26a948cca8cf2f5
NotNott <64883994+NotNott@users.noreply.github.com>
2020-05-11 05:05:08 -0700

    This is my new stuff, no color command

---
+++ color.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name: 'color',
+    description: '',
+    execute(message, args) {}}
--- index.js
+++ index.js
@@ -1,58 +1,66 @@
 const discord = require ('discord.js');
 const bot = new discord.Client();
 const {prefix, token} = require('./config.json');
-const {help} = require ('./help.js');
+
 const fs = require('fs');
+//client.commands = new Discord.collection();
 
 var helpList = fs.readFileSync('./Helplist.txt', 'utf-8');
 
-bot.login(token);
-//help menu
-bot.on(help)
-    
-    //'message', (message) => {
-//    if (!message.content.startsWith(prefix) || message.author.bot) return;
-//    const args = message.content.slice(prefix.length).split(' ');
- //   const command = args.shift().toLowerCase();
-//
-//    if (command == 'help'){
-//
- //   return message.channel.send (helpList);
-//}})
-//create roles
-bot.on('message', (message, guild) => {
-    if (!message.content.startsWith(prefix) || message.author.bot || !message.member.hasPermission('MANAGE_ROLES')) return;
+bot.commands = new discord.Collection();
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	bot.commands.set(command.name, command);
+}
+
+
+bot.login(token);
+
+//help menu
+bot.on('message', (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(' ');
     const command = args.shift().toLowerCase();
 
-    if (command == 'create' && !args[1]){
-        
-        message.guild.roles.create(
-        {
-            data: {
-              name : args[0],
-              color : Math.floor(Math.random() * 16777214) + 1,        
-        },
-            reason: 'We are Lazy.',       
-        })
-            .then(console.log)
-            .catch(console.error);
-            return message.reply ('You have made a new role.')}
-    if (command == 'create'){
-        
-        message.guild.roles.create(
-                {
-            data: {
-                name : args[0] + ' ' + args[1],
-                color : Math.floor(Math.random() * 16777214) + 1,        
-                },
-            reason: 'We are Lazy.',       
-                })
-                    .then(console.log)
-                    .catch(console.error);
-                    return message.reply ('You have made a new role.')
+    if (command == 'help'){
+
+    return message.channel.send (helpList);
 }});
+//create roles
+
+bot.on('message', (message, guild) => {
+    if (!message.content.startsWith(prefix) || message.author.bot || !message.member.hasPermission('MANAGE_ROLES')) return;
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command == 'create'){
+      bot.commands.get('create').execute(message, args);}
+
+    else if (command == 'give'){
+      bot.commands.get('give').execute(message, args);}
+        
+    else if (command == 'take'){
+      bot.commands.get('take').execute(message, args);}
+          
+    else if (command == 'delete'){
+      bot.commands.get('delete').execute(message, args);}
+            
+    else if (command == 'add'){
+      bot.commands.get('add').execute(message, args);}
+
+    else if (command == 'remove'){
+      bot.commands.get('remove').execute(message, args);}  
+
+    else if (command == 'perm'){
+      bot.commands.get('perm').execute(message, args);}
+
+    //else if (command == 'color'){
+      //bot.commands.get('color').execute(message, args);}
+       
+})
 //list roles
 bot.on('message', (message, guild) => {
     if (!message.content.startsWith(prefix) || message.author.bot || !message.member.hasPermission('MANAGE_ROLES')) return;
@@ -70,130 +78,115 @@ bot.on('message', (message, guild) => {
 
         }   
     }
-})
-//add roles to user
-bot.on('message', (message, guild) => {
-    if (!message.content.startsWith(prefix) || message.author.bot || !message.member.hasPermission('MANAGE_ROLES')) return;
-    
-        const args = message.content.slice(prefix.length).split(' ');
-        const command = args.shift().toLowerCase();
-    
-    if (command == "give" && !args[2])  { 
-        //if (args.length == 1){
-        var user = message.mentions.members.first();
-        var role= message.member.guild.roles.cache.find(role => role.name === args[0]);
-        if(!role) return message.reply(args[0] + " aint a roll");
-        if(!user) return message.reply ("Aint nobody named that.");
-         
-        else user.roles.add(role);
-          return message.reply ("Role Be There.")}
-
-    if (command == "give")  { 
-        
-        var user = message.mentions.members.first();
-        var role= message.member.guild.roles.cache.find(role => role.name === args[0] + ' ' + args[1]);
-        if(!role) return message.reply(args[0] +' '+ args[1]+ " aint a roll");
-        if(!user) return message.reply ("Aint nobody named that.");
-             
-        else user.roles.add(role);
-        return message.reply ("Role Be There.")
-    
-    
-        }});
-//remove roles form user
-bot.on('message', (message, guild, client, ) => {
-    if (!message.content.startsWith(prefix) || message.author.bot || !message.member.hasPermission('MANAGE_ROLES')) return;
-    
-        const args = message.content.slice(prefix.length).split(' ');
-        const command = args.shift().toLowerCase();
-
-    if (command == "take" && !args[2]){
-        var user = message.mentions.members.first();
-        var role= message.member.guild.roles.cache.find(role => role.name === args[0]);
-        if(!role) return message.reply(args[0] + " aint a roll");
-        if(!user) return message.reply ("Aint nobody named that.");
-         
-        else user.roles.remove(role);
-          return message.reply ("Role Be Gone.")}
-
-          if (command == "take"){
-            var user = message.mentions.members.first();
-            var role= message.member.guild.roles.cache.find(role => role.name === args[0] + ' ' + args[1]);
-            if(!role) return message.reply(args[0] +' '+ args[1] + " aint a roll");
-            if(!user) return message.reply ("Aint nobody named that.");
-             
-            else user.roles.remove(role);
-              return message.reply ("Role Be Gone.")
-           
-       
-}});
-//delete roles
-bot.on('message', (message, guild, client, ) => {
-    if (!message.content.startsWith(prefix) || message.author.bot || !message.member.hasPermission('MANAGE_ROLES')) return;
-    
-        const args = message.content.slice(prefix.length).split(' ');
-        const command = args.shift().toLowerCase();
-
-        if (command == "delete" && !args[1]) {
-            var role= message.member.guild.roles.cache.find(role => role.name === args[0]);
-            if(!role) return message.reply(args[0] + " aint a roll");
-
-            else role.delete();
-            return message.reply("deleted " + args[0])}
-
-            if (command == "delete" ) {
-                var role= message.member.guild.roles.cache.find(role => role.name === args[0] + ' ' + args[1]);
-                if(!role) return message.reply(args[0] + ' ' + args[1] + " aint a roll");
-    
-                else role.delete();
-                return message.reply("deleted " + args[0] + ' ' + args[1])
-
-        }
-
-
-
-})
-//add roles to self
+});
+            
 bot.on('message', (message, guild, client, ) => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     
         const args = message.content.slice(prefix.length).split(' ');
         const command = args.shift().toLowerCase();
 
-    if (command == "add" && !args[1]){
-        var role= message.member.guild.roles.cache.find(role => role.name === args[0]);
-        if(!role) return message.reply(args[0] + " aint a roll");
-         
-        else message.member.roles.add(role);
-          return message.reply ("Role be on you.")}
-
-          if (command == "add"){
-            var role= message.member.guild.roles.cache.find(role => role.name === args[0] + ' ' + args[1]);
-            if(!role) return message.reply(args[0] + ' ' + args[1] + " aint a roll");
-             
-            else message.member.roles.add(role);
-              return message.reply ("Role be on you.")
-       
-}});
-//remove roles from self
-bot.on('message', (message, guild, client, ) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
-        const args = message.content.slice(prefix.length).split(' ');
-        const command = args.shift().toLowerCase();
-
-    if (command == "remove" && !args[1]){
+    if (command == "blue" && !args[1]){
         var role= message.member.guild.roles.cache.find(role => role.name === args[0]);
         if(!role) return message.reply(args[0] + " aint a roll");
         
-        else message.member.roles.remove(role);
-          return message.reply ("Role be gone from you.")}
+        else role.edit( {color : '#0000ff' })
+          return message.reply ("Role has blue")}
 
-          if (command == "remove"){
+          if (command == "blue"){
             var role= message.member.guild.roles.cache.find(role => role.name === args[0] + ' ' + args[1]);
             if(!role) return message.reply(args[0] + ' ' + args[1] + " aint a roll");
             
-            else message.member.roles.remove(role);
-              return message.reply ("Role be gone from you.")
+            else role.edit( {color : '#0000ff' })
+              return message.reply ("Role has blue")
        
+              
+}});
+bot.on('message', (message, guild, client, ) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+        const args = message.content.slice(prefix.length).split(' ');
+        const command = args.shift().toLowerCase();
+
+    if (command == "red" && !args[1]){
+        var role= message.member.guild.roles.cache.find(role => role.name === args[0]);
+        if(!role) return message.reply(args[0] + " aint a roll");
+        
+        else role.edit( {color : '#FF0000' })
+          return message.reply ("Role has red")}
+
+          if (command == "red"){
+            var role= message.member.guild.roles.cache.find(role => role.name === args[0] + ' ' + args[1]);
+            if(!role) return message.reply(args[0] + ' ' + args[1] + " aint a roll");
+            
+            else role.edit( {color : '#FF0000' })
+              return message.reply ("Role has red")
+       
+              
+}});
+bot.on('message', (message, guild, client, ) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+        const args = message.content.slice(prefix.length).split(' ');
+        const command = args.shift().toLowerCase();
+
+    if (command == "green" && !args[1]){
+        var role= message.member.guild.roles.cache.find(role => role.name === args[0]);
+        if(!role) return message.reply(args[0] + " aint a roll");
+        
+        else role.edit( {color : '#008000' })
+          return message.reply ("Role has green")}
+
+          if (command == "green"){
+            var role= message.member.guild.roles.cache.find(role => role.name === args[0] + ' ' + args[1]);
+            if(!role) return message.reply(args[0] + ' ' + args[1] + " aint a roll");
+            
+            else role.edit( {color : '#008000' })
+              return message.reply ("Role has green")
+       
+
+}});
+bot.on('message', (message, guild, client, ) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+        const args = message.content.slice(prefix.length).split(' ');
+        const command = args.shift().toLowerCase();
+
+    if (command == "yellow" && !args[1]){
+        var role= message.member.guild.roles.cache.find(role => role.name === args[0]);
+        if(!role) return message.reply(args[0] + " aint a roll");
+        
+        else role.edit( {color : '#FFFF00' })
+          return message.reply ("Role has yellow")}
+
+          if (command == "yellow"){
+            var role= message.member.guild.roles.cache.find(role => role.name === args[0] + ' ' + args[1]);
+            if(!role) return message.reply(args[0] + ' ' + args[1] + " aint a roll");
+            
+            else role.edit( {color : '#FFFF00' })
+              return message.reply ("Role has yellow")
+       
+              
+}});
+bot.on('message', (message, guild, client, ) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+        const args = message.content.slice(prefix.length).split(' ');
+        const command = args.shift().toLowerCase();
+
+    if (command == "pink" && !args[1]){
+        var role= message.member.guild.roles.cache.find(role => role.name === args[0]);
+        if(!role) return message.reply(args[0] + " aint a roll");
+        
+        else role.edit( {color : '#FF69B4' })
+          return message.reply ("Role has Hot Pink")}
+
+          if (command == "pink"){
+            var role= message.member.guild.roles.cache.find(role => role.name === args[0] + ' ' + args[1]);
+            if(!role) return message.reply(args[0] + ' ' + args[1] + " aint a roll");
+            
+            else role.edit( {color : '##FF69B4' })
+              return message.reply ("Role has Hot Pink")
+       
+              
 }});
