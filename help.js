4dea34dc2c961a858ae4acc8d1ba03f1bf649135
NotNott <64883994+NotNott@users.noreply.github.com>
2020-05-14 12:20:17 -0700

    Delete help.js

--- help.js
+++
@@ -1,9 +0,0 @@
-function help()
-('message', (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
-
-    if (command == 'help'){
-
-    return message.channel.send (helpList)}})
